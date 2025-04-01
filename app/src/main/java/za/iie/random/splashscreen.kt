6fb4e30b75679567910796aafbef51b50f52c746
alwa17 <st10447049@vcconnect.edu.za>
2025-04-01 19:14:47 +0200

    trying different ways to get this working

--- app/src/main/java/za/iie/random/splashscreen.kt
+++ app/src/main/java/za/iie/random/splashscreen.kt
@@ -1,12 +1,13 @@
 package za.iie.random
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class splashscreen : AppCompatActivity() {
+class splashscreen<TextView> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +17,28 @@ class splashscreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val editText = findViewById<EditText>(R.id.editText)
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
+        val textDisplay = findViewById<TextView>(R.id.textView)
+
+        buttonSearch.setOnClickListener {
+            val timeOfDay = editText.text.toString().trim()
+            val suggestion = when (timeOfDay.lowercase()) {
+                "morning" -> "HAVE A HEALTHY BREAKFAST"
+                "mid-morning" -> "HAVE SOMETHING LIGHT TO CONTINUE YOUR DAY"
+                "afternoon" -> "HAVE SOMETHING TO FILL YOU UP FOR THE AFTERNOON"
+                "mid-afternoon" -> "GET YOURSELF A SNACK AND A DRINK TO HOLD UP"
+                "dinner" -> "ENJOY A HOT MEAL WITH YOUR FAM BAM! "
+                else -> "PLEASE ENTER CORRECT SPELLING."
+            }
+            textDisplay.text = suggestion
+        }
+
+        buttonReset.setOnClickListener {
+            editText.text.clear()
+            textDisplay.text = ""
+        }
+    }
     }
 }
